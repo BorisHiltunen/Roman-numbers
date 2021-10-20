a5fe53ec1ea51c5695f6686b3b8116371feb0d1a
BorisHiltunen <90689146+BorisHiltunen@users.noreply.github.com>
2021-10-20 11:51:09 +0300

    Add files changes to 1000

--- Roman_numerals.py
+++ Roman_numerals.py
@@ -1,36 +1,11 @@
 class App:
     
-    def roman_Numbers2(n):
-        if n < 4:
-            answer = ""
-            for number in range(n):
-                answer += "I"
-            return answer
-        if n == 4:
-            return "IV"
-        if n < 9:
-            answer = "V"
-            for number in range(n-5):
-                answer += "I"
-            return answer
-        # ei tulosta oikein
-        if n < 11:
-            answer = "IXX"
-            for number in range(n-9):
-                answer[1:]
-            return answer
-        if n == 9:
-            return "IX"
-        if n == 10:
-            return "X"
-    
     def roman_Numbers(n):
         roman = "IIIVIIIX"
         answer = ""
         list = []
         list1 = ["I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX", "X"]
         list2 = [roman[:-7], roman[:-6], roman[:-5], roman[2:-4], roman[3:-4], roman[3:-3], roman[3:-2], roman[3:-1], roman[6:], roman[7:]]
-        print(list2)
         if n == 0:
             return None
         if n < 11:
@@ -75,24 +50,57 @@ class App:
             return "CC"
         if n == 300:
             return "CCC"
-        if n < 399:
+        if n < 400:
             combo = ""
             for number in str(n):
                 list.append(int(number))
             combo = str(list[1]) + str(list[2])
             answer += ("C" * list[0]) + App.roman_Numbers(int(combo))
             return answer
-
-
+        if n == 400:
+            return "CD"
+        if n < 500:
+            combo = ""
+            for number in str(n):
+                list.append(int(number))
+            combo = str(list[1]) + str(list[2])
+            answer += "CD" + App.roman_Numbers(int(combo))
+            return answer
+        if n == 500:
+            return "D"
+        if n == 600:
+            return "DC"
+        if n == 700:
+            return "DCC"
+        if n == 800:
+            return "DCCC"
+        if n < 900:
+            combo = ""
+            for number in str(n):
+                list.append(int(number))
+            combo = str(list[1]) + str(list[2])
+            answer += "D" + ("C" * (list[0]-5)) + App.roman_Numbers(int(combo))
+            return answer
+        if n == 900:
+            return "CM"
+        if n < 1000:
+            combo = ""
+            for number in str(n):
+                list.append(int(number))
+            combo = str(list[1]) + str(list[2])
+            answer += "CM" + App.roman_Numbers(int(combo))
+            return answer
+        if n == 1000:
+            return "M"
 
     def roman_list(n: list):
-        #for number in list:
-
-        return "o"
-
+        new_list = []
+        for number in n:
+            new_list.append((number, App.roman_Numbers(number)))
+        return new_list
 
 if __name__ == "__main__":
-    count = 320
+    count = 876
+    list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
     print(App.roman_Numbers(count))
-    #print(App.roman_Numbers(9))
-    #print(App.roman_list([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]))
+    print(App.roman_list(list))
